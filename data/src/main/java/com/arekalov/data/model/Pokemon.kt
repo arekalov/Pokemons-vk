7322c4ec1c8b4b2a1987d1ec390f93e57cf46c47
arekalov <artyom.rekalov@gmail.com>
2024-05-11 11:25:58 +0300

    add fragment_pokemon_detail.xml

--- data/src/main/java/com/arekalov/data/model/Pokemon.kt
+++ data/src/main/java/com/arekalov/data/model/Pokemon.kt
@@ -4,9 +4,9 @@ data class Pokemon(
     val name: String,
     val id: Int,
     val base_experience: Int,
+    val is_default: Boolean,
     val height: Int,
     val weight: Int,
-    val is_default: Boolean,
     val order: Int,
     val sprites: Sprites,
 )
